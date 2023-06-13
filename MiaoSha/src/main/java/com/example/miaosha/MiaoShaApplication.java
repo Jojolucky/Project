b3af4fbf32fbe0a42f28a7b2e5a2b22d229ffb28
NCUTemp <jiaohelucky@gmail.com>
2023-06-13 15:31:08 -0700

    MiaoSha

---
+++ MiaoSha/src/main/java/com/example/miaosha/MiaoShaApplication.java
@@ -0,0 +1,13 @@
+package com.example.miaosha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaoShaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiaoShaApplication.class, args);
+    }
+
+}
---
+++ MiaoSha/src/test/java/com/example/miaosha/MiaoShaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miaosha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaoShaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
