d187d9de5661aa5d09ea4b96af7abec6dc9b5747
NCUTemp <jiaohelucky@gmail.com>
2023-06-26 18:04:47 -0700

    md5

--- MemeriesProject/server/index.js
+++ MemeriesProject/server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
-import BodyParser from "body-parser";
-import Cors from "cors";
+import bodyParser from "body-parser";
+import cors from "cors";
 import bodyParser from "body-parser";
 
 const app = express();
@@ -12,7 +12,7 @@ app.use(cors());
 
 // http://www.mongodb.com/cloud/atlas
 const CONNECTION_URL =
-  "mongodb+srv://memorise:memorise1234@cluster0.aock4mv.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://memorise:m@cluster0.aock4mv.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedtopology: true })
