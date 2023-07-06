c6aea75b60db16a473bcfe7cc505760d173d2b49
NCUTemp <jiaohelucky@gmail.com>
2023-07-05 17:14:55 -0700

    update the prameter used in the login. Use User object

--- Booking/src/main/java/com/example/booking/controller/GoodsController.java
+++ Booking/src/main/java/com/example/booking/controller/GoodsController.java
@@ -1,8 +1,11 @@
 package com.example.booking.controller;
 
-
 import com.example.booking.pojo.User;
+import com.example.booking.service.IUserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -10,21 +13,31 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * 商品 ** @author zhoubin * @since 1.0.0
+ * 商品 ** @author zhoubin
+ * * @since 1.0.0
  */
 @Controller
 @RequestMapping("/goods")
 public class GoodsController {
-    /*** 跳转登录页 ** @return */
+    @Autowired
+    private IUserService userService;
+
+    /**
+     * 跳转登录页 *
+     *
+     * @return
+     */
     @RequestMapping("/toList")
-    public String toList(HttpSession session, Model model, @CookieValue("userTicket") String ticket) {
-        if (StringUtils.isEmpty(ticket)) {
-            return "login";
-        }
-        User user = (User) session.getAttribute(ticket);
-        if (null == user) {
-            return "login";
-        }
+    public String toList(Model model,User user){
+//    public String toList(HttpServletRequest request, HttpServletResponse response, Model model, @CookieValue("userTicket") String ticket) {
+//        if (StringUtils.isEmpty(ticket)) {
+//            return "login";
+//        }
+////        User user = (User) session.getAttribute(ticket);
+//        User user = userService.getByUserCookie(ticket, request, response);
+//        if (null == user) {
+//            return "login";
+//        }
         model.addAttribute("user", user);
         return "goodsList";
     }
--- Booking/src/main/java/com/example/booking/service/impl/UserServiceImpl.java
+++ Booking/src/main/java/com/example/booking/service/impl/UserServiceImpl.java
@@ -65,21 +65,21 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         String ticket = UUIDUtil.uuid();
         // 写入redis
         redisTemplate.opsForValue().set("user:" + ticket, user);
-        request.getSession().setAttribute(ticket, user);
+//        request.getSession().setAttribute(ticket, user);
         CookieUtil.setCookie(request, response, "userTicket", ticket);
         return RespBean.success(ticket);
     }
 
-//    @Override
-//    public User getByUserCookie(String userTicket, HttpServletRequest request, HttpServletResponse response) {
-//        if (StringUtils.isEmpty(userTicket)) {
-//            return null;
-//        }
-//        User user = (User)redisTemplate.opsForValue().get("user:" + userTicket);
-//        if(user != null){
-//            CookieUtil.setCookie(request, response, "userTicker", userTicket);
-//        }
-//        return user;
-//
-//    }
+    @Override
+    public User getByUserCookie(String userTicket, HttpServletRequest request, HttpServletResponse response) {
+        if (StringUtils.isEmpty(userTicket)) {
+            return null;
+        }
+        User user = (User)redisTemplate.opsForValue().get("user:" + userTicket);
+        if(user != null){
+            CookieUtil.setCookie(request, response, "userTicker", userTicket);
+        }
+        return user;
+
+    }
 }
