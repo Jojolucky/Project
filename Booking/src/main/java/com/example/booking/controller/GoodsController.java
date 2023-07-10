35ddc997809e31909752350fe433612a3254ae27
NCUTemp <jiaohelucky@gmail.com>
2023-07-10 12:04:13 -0700

    goods detail page

--- Booking/src/main/java/com/example/booking/controller/GoodsController.java
+++ Booking/src/main/java/com/example/booking/controller/GoodsController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -47,5 +48,14 @@ public class GoodsController {
         model.addAttribute("goodsList", goodsService.findGoodsVo());
 
         return "goodsList";
+    }
+    /**
+     * 跳转商品详情页
+     */
+    @RequestMapping("/toDetail/{goodsId}")
+    public String toDetail(Model model, User user, @PathVariable Long goodsId) {
+        model.addAttribute("user", user);
+        model.addAttribute("goods",goodsService.findGoodsVoByGoodsId(goodsId));
+        return "goodsDetail";
     }
 }
