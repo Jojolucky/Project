7cf45842fac3a827f328f2b904fa277980157dbc
NCUTemp <jiaohelucky@gmail.com>
2023-07-19 00:44:10 -0700

    update password function

--- Booking/src/main/java/com/example/booking/vo/RespBeanEnum.java
+++ Booking/src/main/java/com/example/booking/vo/RespBeanEnum.java
@@ -22,10 +22,12 @@ public enum RespBeanEnum {
     LOGIN_ERROR(500210,"用户名或者密码错误"),
     MOBILE_ERROR(500211,"手机号码格式错误"),
     BIND_ERROR(500212,"参数校验异常" ),
-    // 秒杀模块5005xx
-    REPEATE_ERROR(500501,"商品每人限购一件"),
+    EMPTY_STOCK(500212,"参数校验异常" ),
+    MOBILE_NOT_EXIST(500213, "手机号码不存在"),
+    PASSWORD_UPDATE_FAIL(500214, "密码更新失败"),
 
-    EMPTY_STOCK(500212,"参数校验异常" );
+    // 秒杀模块5005xx
+    REPEATE_ERROR(500501,"商品每人限购一件");
 
 
     private final Integer code;
